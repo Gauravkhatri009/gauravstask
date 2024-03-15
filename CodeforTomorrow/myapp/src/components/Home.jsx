9ff3b35d31df49fe4cdcfe61364f9ea3576eb4b3
Gauravkhatri009 <163546438+Gauravkhatri009@users.noreply.github.com>
2024-03-15 18:59:27 +0530

    Add files via upload

---
+++ CodeforTomorrow/myapp/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Sidebar from './Sidebar'
+
+const Home = () => {
+  return (
+    <Box>
+       <Sidebar /> 
+    </Box>
+  )
+}
+
+export default Home
